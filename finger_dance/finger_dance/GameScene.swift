6d2ef837c481f6c7eb7b268a1df68bb089c13292
ezioruan <qiaoqinqie2@gmail.com>
2015-11-18 17:48:49 +0800

    add figuar dance

---
+++ finger_dance/finger_dance/GameScene.swift
@@ -0,0 +1,45 @@
+//
+//  GameScene.swift
+//  finger_dance
+//
+//  Created by ezioruan on 15/11/18.
+//  Copyright (c) 2015年 ezioruan. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
+        myLabel.text = "Hello, World!";
+        myLabel.fontSize = 45;
+        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+        
+        self.addChild(myLabel)
+    }
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+       /* Called when a touch begins */
+        
+        for touch in touches {
+            let location = touch.locationInNode(self)
+            
+            //let sprite = SKSpriteNode(imageNamed:"Spaceship")
+            
+            let sprite =  SKShapeNode(rectOfSize: CGSize(width: 300, height: 100))
+            sprite.fillColor = SKColor.blueColor()
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            
+            self.addChild(sprite)
+        }
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
---
+++ finger_dance/finger_danceTests/finger_danceTests.swift
@@ -0,0 +1,36 @@
+//
+//  finger_danceTests.swift
+//  finger_danceTests
+//
+//  Created by ezioruan on 15/11/18.
+//  Copyright © 2015年 ezioruan. All rights reserved.
+//
+
+import XCTest
+@testable import finger_dance
+
+class finger_danceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
